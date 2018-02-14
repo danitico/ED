7d436f1e4549013e22886db393f4ace26fbbb6c2
Daniel Ranchal <danitico98@gmail.com>
2018-02-15 00:21:06 +0100

    Borrando

--- version1/html/search/functions_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['crossproduct',['crossProduct',['../classed_1_1Vector3D.html#add27a5e5bc5d17ccd9bff96ef057f7c6',1,'ed::Vector3D']]]
-];
--- version1/html/search/functions_9.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['productomixto',['productoMixto',['../classed_1_1Vector3D.html#ad5a534ac68797097e297ffe9baf20d27',1,'ed::Vector3D']]]
-];
