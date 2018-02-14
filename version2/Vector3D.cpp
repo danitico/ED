bb434023e82e74c4511c549d33da4e90aa74d530
Daniel Ranchal <danitico98@gmail.com>
2018-02-15 00:17:49 +0100

    Preparando la version 2

--- version2/Vector3D.cpp
+++ version2/Vector3D.cpp
@@ -2,81 +2,247 @@
    \file Vector3D.cpp
    \brief Fichero que contiene el código de las funciones de la clase Vector3D
 */
-
-
-// Ficheros de cabecera
 #include <iostream>
-
-// Para usar sqrt, acos
 #include <cmath>
-
-// Para controlar las pre y post condiciones mediante asertos
 #include <cassert>
-
 #include "Vector3D.hpp"
-
 // OBSERVADORES
-
-// COMPLETAR
-
-
+double ed::Vector3D::modulo() const{
+	double valor=sqrt(v1_*v1_ + v2_*v2_ + v3_*v3_);
+	#ifndef NDEBUG
+	assert(abs(valor-sqrt(v1_*v1_ + v2_*v2_ + v3_*v3_))<=COTA_ERROR);
+	#endif
+	return valor;
+}
+double ed::Vector3D::angulo(ed::Vector3D v) const{
+	#ifndef NDEBUG
+	assert((modulo()*v.modulo())>0);
+	#endif
+	double valor=acos(dotProduct(v)/(modulo() * v.modulo()));
+	#ifndef NDEBUG
+	assert(abs(valor - acos(dotProduct(v)/(modulo() * v.modulo())))<=COTA_ERROR);
+	#endif
+	return valor;
+}
+double ed::Vector3D::alfa() const{
+	#ifndef NDEBUG
+	assert(modulo()>0);
+	#endif
+	ed::Vector3D a(1,0,0);
+	double res=angulo(a);
+	#ifndef NDEBUG
+	assert(abs(res - angulo(a))<=COTA_ERROR);
+	#endif
+	return res;
+}
+double ed::Vector3D::beta() const{
+	#ifndef NDEBUG
+	assert(modulo()>0);
+	#endif
+	ed::Vector3D a(0,1,0);
+	double res=angulo(a);
+	#ifndef NDEBUG
+	assert(abs(res - angulo(a))<=COTA_ERROR);
+	#endif
+	return res;
+}
+double ed::Vector3D::gamma() const{
+	#ifndef NDEBUG
+	assert(modulo()>0);
+	#endif
+	ed::Vector3D a(0,0,1);
+	double res=angulo(a);
+	#ifndef NDEBUG
+	assert(abs(res - angulo(a))<=COTA_ERROR);
+	#endif
+	return res;
+}
+double ed::Vector3D::dotProduct(ed::Vector3D v) const{
+	double res=v1_*v.get1() + v2_*v.get2() + v3_*v.get3();
+	#ifndef NDEBUG
+	assert(abs(res - (v1_*v.get1() + v2_*v.get2() + v3_*v.get3()))<=COTA_ERROR);
+	#endif
+	return res;
+}
+ed::Vector3D ed::Vector3D::crossProduct(ed::Vector3D v) const{
+	double w1, w2, w3;
+	
+	w1=get2()*v.get3() - get3()*v.get2();
+	w2=get3()*v.get1() - get1()*v.get3();
+	w3=get1()*v.get2() - get2()*v.get1();
+	
+	ed::Vector3D w(w1,w2,w3);
+	#ifndef NDEBUG
+	assert(dotProduct(w)==0 && v.dotProduct(w)==0 && (abs(w.modulo() - modulo()*v.modulo()*sin(angulo(v)))<=COTA_ERROR));
+	#endif
+	return w;
+}	
+double ed::Vector3D::productoMixto(ed::Vector3D v, ed::Vector3D w) const{
+	double res=dotProduct(v.crossProduct(w));
+	#ifndef NDEBUG
+	assert(abs(res - dotProduct(v.crossProduct(w)))<=COTA_ERROR);
+	#endif
+	return res;
+}
 ////////////////////////////////////////////////////////////////
 
 // MODIFICADORES
-
-// COMPLETAR
-
-
-
+void ed::Vector3D::sumConst(double k){
+	ed::Vector3D aux(*this);
+	set1(get1() + k);
+	set2(get2() + k);
+	set3(get3() + k);
+	#ifndef NDEBUG
+	assert(abs(get1() - (aux.get1() + k))<=COTA_ERROR && abs(get2() - (aux.get2() + k))<=COTA_ERROR && abs(get3() - (aux.get3() + k))<=COTA_ERROR);
+	#endif
+}
+void ed::Vector3D::sumVect(Vector3D v){
+	ed::Vector3D aux(*this);	
+	set1(get1() + v.get1());
+	set2(get2() + v.get2());
+	set3(get3() + v.get3());
+	#ifndef NDEBUG
+	assert(abs(get1() - (aux.get1() + v.get1()))<=COTA_ERROR && abs(get2() - (aux.get2() + v.get2()))<=COTA_ERROR && abs(get3() - (aux.get3() + v.get3()))<=COTA_ERROR);
+	#endif
+}
+void ed::Vector3D::multConst(double k){
+	ed::Vector3D aux(*this);
+	set1(get1() * k);
+	set2(get2() * k);
+	set3(get3() * k);
+	#ifndef NDEBUG
+	assert(abs(get1() - (aux.get1() * k))<=COTA_ERROR && abs(get2() - (aux.get2() * k))<=COTA_ERROR && abs(get3() - (aux.get3() * k))<=COTA_ERROR);
+	#endif
+}
+void ed::Vector3D::multVect(Vector3D v){
+	ed::Vector3D aux(*this);
+	set1(get1() * v.get1());
+	set2(get2() * v.get2());
+	set3(get3() * v.get3());
+	#ifndef NDEBUG
+	assert(abs(get1() - (aux.get1() * v.get1()))<=COTA_ERROR && abs(get2() - (aux.get2() * v.get2()))<=COTA_ERROR && abs(get3() - (aux.get3() * v.get3()))<=COTA_ERROR);
+	#endif
+}
 ////////////////////////////////////////////////////////////////////////////////
-
 // OPERADORES
-
-// COMPLETAR
-
-
-
+bool ed::Vector3D::operator==(Vector3D const &objeto) const{
+	bool res;	
+	if(get1()==objeto.get1() && get2()==objeto.get2() && get3()==objeto.get3()){res=true;}
+	else{res=false;}
+	#ifndef NDEBUG
+	assert(res==(abs(get1()-objeto.get1())<=COTA_ERROR && abs(get2()-objeto.get2())<=COTA_ERROR && abs(get3()-objeto.get3())<=COTA_ERROR));
+	#endif
+	return res;
+}
+ed::Vector3D& ed::Vector3D::operator=(Vector3D const &objeto){
+	set1(objeto.get1());
+	set2(objeto.get2());
+	set3(objeto.get3());
+	#ifndef NDEBUG
+	assert(abs(get1()-objeto.get1())<=COTA_ERROR && abs(get2()-objeto.get2())<=COTA_ERROR && abs(get3()-objeto.get3())<=COTA_ERROR);
+	#endif
+	return *this;
+}
+ed::Vector3D& ed::Vector3D::operator+(Vector3D const &objeto) const{
+	ed::Vector3D *res=new ed::Vector3D();
+	res->set1(get1() + objeto.get1());
+	res->set2(get2() + objeto.get2());
+	res->set3(get3() + objeto.get3());
+	#ifndef NDEBUG
+	assert(abs(res->get1()-(get1() + objeto.get1()))<=COTA_ERROR && abs(res->get2()-(get2() + objeto.get2()))<=COTA_ERROR && abs(res->get3()-(get3() + objeto.get3()))<=COTA_ERROR);
+	#endif
+	return *res;
+}
+ed::Vector3D& ed::Vector3D::operator+() const{
+	ed::Vector3D *res=new ed::Vector3D(*this);
+	#ifndef NDEBUG
+	assert(abs(res->get1()-get1())<=COTA_ERROR && abs(res->get2()-get2())<=COTA_ERROR && abs(res->get3()-get3())<=COTA_ERROR);
+	#endif
+	return *res;
+}
+ed::Vector3D& ed::Vector3D::operator-(Vector3D const &objeto) const{
+	ed::Vector3D *res=new ed::Vector3D();
+	res->set1(get1() - objeto.get1());
+	res->set2(get2() - objeto.get2());
+	res->set3(get3() - objeto.get3());
+	#ifndef NDEBUG
+	assert(abs(res->get1()-(get1() - objeto.get1()))<=COTA_ERROR && abs(res->get2()-(get2() - objeto.get2()))<=COTA_ERROR && abs(res->get3()-(get3() - objeto.get3()))<=COTA_ERROR);
+	#endif	
+	return *res;
+}
+ed::Vector3D& ed::Vector3D::operator-() const{
+	ed::Vector3D *res=new ed::Vector3D(*this);
+	res->set1(-(res->get1()));
+	res->set2(-(res->get2()));
+	res->set3(-(res->get3()));
+	#ifndef NDEBUG
+	assert(abs(res->get1()+get1())<=COTA_ERROR && abs(res->get2()+get2())<=COTA_ERROR && abs(res->get3()+get3())<=COTA_ERROR);
+	#endif	
+	return *res;
+}
+ed::Vector3D& ed::Vector3D::operator*(double k) const{
+	ed::Vector3D *res=new ed::Vector3D();
+	res->set1(get1() * k);
+	res->set2(get2() * k);
+	res->set3(get3() * k);
+	#ifndef NDEBUG
+	assert(abs(res->get1()-(get1()*k))<=COTA_ERROR && abs(res->get2()-(get2()*k))<=COTA_ERROR && abs(res->get3()-(get3()*k))<=COTA_ERROR);
+	#endif
+	return *res;
+}
+double ed::Vector3D::operator*(Vector3D const &objeto) const{
+	return dotProduct(objeto);
+}
+ed::Vector3D& ed::Vector3D::operator^(Vector3D const &objeto) const{
+	ed::Vector3D *res=new ed::Vector3D();
+	*res=crossProduct(objeto);
+	return *res;
+}
 ////////////////////////////////////////////////////////////////////////////////
 
 // FUNCIONES DE LECTURA Y ESCRITURA
-
-// COMPLETAR
-
-
-////////////////////////////////////////////////////////////////////////////////
-
-
+void ed::Vector3D::escribirVector3D() const{
+	std::cout<<"("<<get1()<<", "<<get2()<<", "<<get3()<<")";
+}
+void ed::Vector3D::leerVector3D(){
+	double v1=0, v2=0, v3=0;	
+	std::cout<<"Primer elemento = ";
+	std::cin>>v1;
+	set1(v1);
+	std::cout<<"Segundo elemento = ";
+	std::cin>>v2;
+	set2(v2);
+	std::cout<<"Segundo elemento = ";
+	std::cin>>v3;
+	set3(v3);
+}
 // Se incluyen los operadores sobrecargados dentro del espacio de nombres de ed
 
 namespace ed{
+	// Producto "por un" escalar (prefijo): k * v
+	ed::Vector3D & operator* (double k, ed::Vector3D const & objeto){
+		ed::Vector3D *res = new ed::Vector3D();
+		res->set1(objeto.get1() * k);
+		res->set2(objeto.get2() * k);
+		res->set3(objeto.get3() * k);
+		#ifndef NDEBUG
+		assert(abs(res->get1()-(objeto.get1()*k))<=COTA_ERROR && abs(res->get2()-(objeto.get2()*k))<=COTA_ERROR && abs(res->get3()-(objeto.get3()*k))<=COTA_ERROR);
+		#endif
 
+		return *res;
+	}
+	// Sobrecarga del operador de salida
+	// Se escriben por pantalla las coordenadas del vector 3D  de la forma (v1, v2, v3)
+	ostream &operator<<(ostream &stream, ed::Vector3D const &objeto){
+		stream<<"("<<objeto.get1()<<", "<<objeto.get2()<<", "<<objeto.get3()<<")";
+		return stream;
+	}
 
-// Producto "por un" escalar (prefijo): k * v
-ed::Vector3D & operator* (double k, ed::Vector3D const & objeto)
-{
-	ed::Vector3D *vectorResultado = new ed::Vector3D();
-
-	// COMPLETAR
-
-	return *vectorResultado;
+	// Sobrecarga del operador de entrada
+	istream &operator>>(istream &stream, ed::Vector3D &objeto){
+		double v1=0, v2=0, v3=0;
+		stream>>v1>>v2>>v3;
+		objeto.set1(v1); objeto.set2(v2); objeto.set3(v3);
+		return stream;
+	}
 }
-
-
-// Sobrecarga del operador de salida
-// Se escriben por pantalla las coordenadas del vector 3D  de la forma (v1, v2, v3)
-ostream &operator<<(ostream &stream, ed::Vector3D const &objeto)
-{
-	// COMPLETAR
-
-  return stream;
-}
-
-// Sobrecarga del operador de entrada
-istream &operator>>(istream &stream, ed::Vector3D &objeto)
-{
-  // COMPLETAR
-
-  return stream;
-} 
-
-} // Fin del espacio de nombres ed
--- version2/funcionesAuxiliares.hpp
+++ version2/funcionesAuxiliares.hpp
@@ -1,54 +1,94 @@
-
 /*!
   \file   funcionesAuxiliares.hpp
   \brief  Funciones auxiliares para el programa principal de la práctica 1
   \author 
   \date  
 */
-
 #ifndef _FUNCIONESAUXILIARES_HPP_
 #define _FUNCIONESAUXILIARES_HPP_
-
 #include "Vector3D.hpp"
-
-
-namespace ed
-{
+namespace ed{
 	/*! 
 		\brief   Muestra el menú del programa principal 
 		\return  Devuelve el número de opción elegido
 	*/
 	int menu();
-
-	// COMPLETAR LOS COMENTARIOS DE DOXYGEN
+	/**
+    * @fn void leerVectores(ed::Vector3D &u, ed::Vector3D &v, ed::Vector3D &w)
+    * @brief Función que invoca a otras relacionadas con la lectura de vectores
+	 * @param u Dato de tipo Vector3D&
+	 * @param v Dato de tipo Vector3D&
+	 * @param w Dato de tipo Vector3D&
+	 *
+	 * Se invocan a las funciones leerVector3D(), y la sobrecarga del operador de entrada
+	 */
 	void leerVectores(ed::Vector3D &u, ed::Vector3D &v, ed::Vector3D &w);
-
-	// COMPLETAR LOS COMENTARIOS DE DOXYGEN
+	/**
+    * @fn void escribirVectores(ed::Vector3D const &u, ed::Vector3D const &v, ed::Vector3D const &w)
+    * @brief Función que invoca a otras relacionadas con la muestra de información de los vectores
+	 * @param u Dato de tipo const Vector3D&
+	 * @param v Dato de tipo const Vector3D&
+	 * @param w Dato de tipo const Vector3D&
+	 *
+	 * Se invocan a las funciones escribirVector3D() y la sobrecarga del operador de salida
+	 */
 	void escribirVectores(ed::Vector3D const &u, ed::Vector3D const &v, ed::Vector3D const &w);
-
-
-	// COMPLETAR LOS COMENTARIOS DE DOXYGEN
+	/**
+    * @fn void observadoresDeVectores(ed::Vector3D const &u, ed::Vector3D const &v)
+    * @brief Función que invoca a los observadores de la clase Vector3D
+	 * @param u Dato de tipo const Vector3D&
+	 * @param v Dato de tipo const Vector3D&
+	 *
+	 * Se invocan a las funciones escribirVector3D(), modulo(), angulo(Vector3D v), get1(), get2(), get3(), alfa(), beta() y gamma().
+	 */
 	void observadoresDeVectores(ed::Vector3D const &u, ed::Vector3D const &v);
-
-	// COMPLETAR LOS COMENTARIOS DE DOXYGEN
+	/**
+    * @fn void modificarVector(ed::Vector3D &u, ed::Vector3D const &v, double k)
+    * @brief Función que invoca a los modificadores de la clase Vector3D
+	 * @param u Dato de tipo Vector3D&
+	 * @param v Dato de tipo const Vector3D&
+	 * @param k Dato de tipo double
+	 *
+	 * Se invocan a las funciones sumConst(double k), multConst(double k), sumVect(Vector3D v) y multVect(Vector3D v)
+	 */
 	void modificarVector(ed::Vector3D &u, ed::Vector3D const &v, double k);
-
-	// COMPLETAR LOS COMENTARIOS DE DOXYGEN
+	/**
+    * @fn void mostrarProductoEscalar(ed::Vector3D const &u, ed::Vector3D const &v)
+    * @brief Función en la que se calcula el producto escalar
+	 * @param u Dato de tipo const Vector3D&
+	 * @param v Dato de tipo const Vector3D&
+	 *
+	 * Se invocan a la funcion dotProduct() y operator*(), mostrando los resultados esperados
+	 */
 	void mostrarProductoEscalar(ed::Vector3D const &u, ed::Vector3D const &v); 
-
-	// COMPLETAR LOS COMENTARIOS DE DOXYGEN
+	/**
+    * @fn void mostrarProductoVectorial(ed::Vector3D const &u, ed::Vector3D const &v)
+    * @brief Función en la que se calcula el producto vectorial
+	 * @param u Dato de tipo const Vector3D&
+	 * @param v Dato de tipo const Vector3D&
+	 *
+	 * Se invocan a la funcion crossProduct() y operator^(), mostrando los resultados esperados
+	 */
 	void mostrarProductoVectorial(ed::Vector3D const &u, ed::Vector3D const &v);
-
-	// COMPLETAR LOS COMENTARIOS DE DOXYGEN
+	/**
+    * @fn void mostrarProductoMixto(ed::Vector3D const &u, ed::Vector3D const &v, ed::Vector3D const &w)
+    * @brief Función en la que se calcula el producto mixto
+	 * @param u Dato de tipo const Vector3D&
+	 * @param v Dato de tipo const Vector3D&
+	 * @param w Dato de tipo const Vector3D&
+	 *
+	 * Se invoca a la funcion productoMixto(Vector3D v, Vector3D w)
+	 */	
 	void mostrarProductoMixto(ed::Vector3D const &u, ed::Vector3D const &v, ed::Vector3D const &w);
-
-	// COMPLETAR LOS COMENTARIOS DE DOXYGEN
+/**
+    * @fn void mostrarOperadores(ed::Vector3D const &u, ed::Vector3D const &v, ed::Vector3D &w,  double k)
+    * @brief Función en la que se llama a los operadores sobrecargados
+	 * @param u Dato de tipo const Vector3D&
+	 * @param v Dato de tipo const Vector3D&
+	 * @param w Dato de tipo const Vector3D&
+	 * @param k Dato de tipo double
+	 */	
 	void mostrarOperadores(ed::Vector3D const &u, ed::Vector3D const &v, ed::Vector3D &w,  double k);
-
-
-
 } // Fin del espacio de nombre de la asignatura: ed
-
 // Fin de _FUNCIONESAUXILIARES_HPP_
 #endif
-
