a3ec6a381f94494704fff93aaeefac65692f0a1d
Daniel Ranchal <danitico98@gmail.com>
2018-02-15 00:19:51 +0100

    Cambio carpeta

---
+++ P1/version1/html/search/searchdata.js
@@ -0,0 +1,33 @@
+var indexSectionsWithContent =
+{
+  0: "_abcdefgilmoprsuvwy",
+  1: "v",
+  2: "e",
+  3: "fmpv",
+  4: "abcdeglmopsv",
+  5: "_bcfgiopruwy",
+  6: "i"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "namespaces",
+  3: "files",
+  4: "functions",
+  5: "defines",
+  6: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "Todo",
+  1: "Clases",
+  2: "Namespaces",
+  3: "Archivos",
+  4: "Funciones",
+  5: "defines",
+  6: "Páginas"
+};
+
