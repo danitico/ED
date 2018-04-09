1e5b44bf9a1ecec81536ef7d544f584252142bed
Daniel@Uco <i62rapad@uco.es>
2018-04-09 17:40:07 +0200

    Tremendo constructor útil

--- P3/MonticuloMediciones.hpp
+++ P3/MonticuloMediciones.hpp
@@ -11,57 +11,46 @@
 #include "Medicion.hpp"
 #include "MonticuloMedicionesInterfaz.hpp"
 // COMPLETAR EL CÓDIGO E INCLUIR LOS COMENTARIOS DE DOXYGEN
-namespace ed
-{
-
+namespace ed{
 // COMPLETAR EL CÓDIGO E INCLUIR LOS COMENTARIOS DE DOXYGEN
+	class MonticuloMediciones : public MonticuloMedicionesInterfaz{
+		private:
+			//! \name Atributos privados de la clase MonticuloMediciones
+			std::vector<Medicion> v;
+			//! \name Métodos privados de la clase MonticuloMediciones
+			ed::Medicion getElement(int i) const;
+			void setElement(int i, Medicion medicion);
+			int getLeftChild(int i) const;
+			int getRightChild(int i) const;
+			int getParent(int i) const;
+			void shiftUp(int i);
+			void shiftDown(int i);
+			bool has(ed::Medicion medicion) const;
+			/////////////////////////////////////////////////////////////////////////////////////
 
-class MonticuloMediciones : public MonticuloMedicionesInterfaz
-{
-	private:
-
-		//! \name Atributos privados de la clase MonticuloMediciones
-
-		// COMPLETAR EL CÓDIGO E INCLUIR LOS COMENTARIOS DE DOXYGEN
-
-
-		//! \name Métodos privados de la clase MonticuloMediciones
-
-		// COMPLETAR EL CÓDIGO E INCLUIR LOS COMENTARIOS DE DOXYGEN
-
-	  /////////////////////////////////////////////////////////////////////////////////////
-
-	//! \name Métodos públicos de la clase MonticuloMediciones
-	public:
-
-		//! \name Constructor
-
-		// COMPLETAR EL CÓDIGO E INCLUIR LOS COMENTARIOS DE DOXYGEN
-
-
-		//! \name Observadores
-
-		// COMPLETAR EL CÓDIGO E INCLUIR LOS COMENTARIOS DE DOXYGEN
-
-		////////////////////////////////////////////////////////////
-
-		//! \name Operaciones de modificación
-
-		// COMPLETAR EL CÓDIGO E INCLUIR LOS COMENTARIOS DE DOXYGEN
-
-
-		//! \name Operadores
-
-		// COMPLETAR
-
-
-		////////////////////////////////////////////////////////////////////
-
-		//! \name Función de escritura
-
-		// COMPLETAR
-
-	}; // Clase MonticuloMediciones
-
+			//! \name Métodos públicos de la clase MonticuloMediciones
+			public:
+				//! \name Constructor
+				inline MonticuloMediciones(){
+					#ifndef NDEBUG
+					assert(isEmpty());
+					#endif
+				}
+				//! \name Observadores
+				bool isEmpty() const;
+				int size() const;
+				ed::Medicion top() const;
+				////////////////////////////////////////////////////////////
+				//! \name Operaciones de modificación
+				void insert(ed::Medicion medicion);
+				void remove();
+				void removeAll();
+				void modify(ed::Medicion medicion);
+				//! \name Operadores
+				ed::MonticuloMediciones& operator=(ed::MonticuloMediciones & const m);
+				////////////////////////////////////////////////////////////////////
+				//! \name Función de escritura
+				void print() const;
+			}; // Clase MonticuloMediciones
 } // Espacio de nombres ed
-#endif // _MONTICULO_MEDICIONES_HPP
+#endif // _MONTICULO_MEDICIONES_HP
