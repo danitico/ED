e4158fa0763fe4254349713fe0ad82a35d7ed043
Daniel@Uco <i62rapad@uco.es>
2018-04-09 16:59:26 +0200

    Terminada la clase interfaz con sus comentarios de doxygen

--- P3/MonticuloMedicionesInterfaz.hpp
+++ P3/MonticuloMedicionesInterfaz.hpp
@@ -7,20 +7,43 @@
 #ifndef _MONTICULO_MEDICIONES_INTERFAZ_HPP
 #define _MONTICULO_MEDICIONES_INTERFAZ_HPP
 #include "Medicion.hpp"
-// COMPLETAR EL CÓDIGO E INCLUIR LOS COMENTARIOS DE DOXYGEN
-namespace ed
-{
-
-	// COMPLETAR EL CÓDIGO E INCLUIR LOS COMENTARIOS DE DOXYGEN
-	class MonticuloMedicionesInterfaz
-	{
+namespace ed{
+	class MonticuloMedicionesInterfaz	{
 		public:
-
-			// COMPLETAR EL CÓDIGO E INCLUIR LOS COMENTARIOS DE DOXYGEN
-
-
+			/**
+			 * @fn virtual bool isEmpty()
+			 * @brief Comprueba si el montículo está vacio.
+			 * @note Función de tipo const
+			 * @note Función virtual pura que debe de ser redefinida en la clase heredera.
+			 * @return Devuelve true si no tiene mediciones y false en el caso contrario.
+			 */
+			virtual bool isEmpty() const = 0;
+			/**
+			 * @fn virtual ed::Medicion top() const
+			 * @brief Devuelve la cima del montículo
+			 * @post isEmpty() == false
+			 * @note Función de tipo const
+			 * @note Función virtual pura que debe de ser redefinida en la clase heredera.
+			 * @return Un objeto de la clase Medicion
+			 */
+			virtual ed::Medicion top() const = 0;
+			/**
+			 * @fn virtual void insert(ed::Medicion medicion)
+			 * @brief Inserta una nueva medición en el montículo
+			 * @param medicion Objeto de la clase Medicion
+			 * @post isEmpty() == false
+			 * @note Función virtual pura que debe de ser redefinida en la clase heredera.
+			 * @return Nada
+			 */
+			virtual void insert(ed::Medicion medicion) = 0;
+			/**
+			 * @fn virtual void remove()
+			 * @brief Borra la medición que ocupa la cima
+			 * @post isEmpty() == false
+			 * @note Función virtual pura que debe de ser redefinida en la clase heredera.
+			 * @return Nada
+			 */
+			virtual void remove() = 0;
 	}; // Clase MonticuloMedicionesInterfaz
-
 } // Espacio de nombres ed
-
 #endif //  _MONTICULO_MEDICIONES_INTERFAZ_HPP
